6ec5e512d5602938ce61c385de4d5be1d058d6fe
Kevin Smith <ksmithbaylor@gmail.com>
2015-05-15 00:52:16 -0400

    initial commit

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.slider').slider({height: 300, indicators: false, interval: 5000});
+
+  });
+})(jQuery);
